a8e3306c8675295024c641eaa27a7274d350d470
ninhoProgrammer <mariohm100293@gmail.com>
2025-05-09 13:56:23 -0600

    Add Vercel Analytics and update layout to include it
    - Added @astrojs/vercel dependency to package.json for Vercel integration.
    - Included @vercel/analytics in Layout.astro to enable analytics tracking.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,17 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true, // set to false when using @vercel/analytics@1.4.0
+    },
+  }),
   integrations: [react()],
 });
